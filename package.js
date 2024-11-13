3023099b4948a81f14a2e005d12b509da2462e48
Adam Wolski <wolas.abc@gmail.com>
2024-11-13 21:21:04 +0100

    fix: bump version

--- package.js
+++ package.js
@@ -1,27 +1,27 @@
 Package.describe({
   name: "wolas:publish-counts",
   summary: "Publish the count of a cursor, in real time",
-  version: "0.5.1",
+  version: "0.6.1",
   git: "https://github.com/percolatestudio/publish-counts.git"
 });
 
-Package.on_use(function (api, where) {
+Package.onUse(function (api, where) {
   api.versionsFrom(['1.6.1', '2.3', '3.0']);
 
   api.use(['blaze', 'templating'], 'client', { weak: true });
   api.use('mongo', 'client');
 
-  api.add_files('publish-counts.js');
+  api.addFiles('publish-counts.js');
 
   api.export('Counts');
   api.export('publishCount', 'server');
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use([
     'wolas:publish-counts',
     'tinytest',
     'facts']);
 
-  api.add_files('publish-counts_tests.js');
+  api.mainModule('publish-counts_tests.js');
 });
